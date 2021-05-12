cdf33c4ecf107ed378a6948ecfb7af5e05993881
cutmeat <johannes.rave@gmail.com>
2021-05-12 09:54:34 +0200

    testing vscode

---
+++ test/svelte/src/lib/BGMap.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import Station from '$lib/Station'
+    import {onMount} from 'svelte';
+    import { stationData } from '$assets/stores'
+
+    let plan;
+    let mapData = {};
+
+    onMount(() => {
+        const stadtteile: HTMLCollection = plan.children;
+
+        for (let stadtteil of stadtteile) {
+            const stadtteilName = stadtteil.classList[1];
+
+            for (let station of stationData[stadtteilName].stations.keys){
+                console.log(station)
+            }
+
+            let newStations = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+
+        }
+    })
+
+
+</script>
+
+
+<style>
+    svg path {
+        fill: #a80c60;
+        stroke: white;
+        stroke-width: 2px;
+    }
+
+    svg path:hover {
+        fill: #DA4567;
+    }
+</style>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1002 558">
+    <g bind:this={plan} class="map_split">
+        <g class="stadtteil west">
+            <path stroke-width="4"
+                  d="M429 293L315.5 503.5h-68l-40-109.5L68 415.5l-65-151L225 244l40.5-147.5 148.5 132 15 64.5z"
+                  class="west background"/>
+        </g>
+        <g class="stadtteil nordost">
+            <path stroke-width="4" d="M566.5 171L669 230.078 798.5 205.5l71 44 106.5-21-100-104L716.5 56 577 111l-10.5 60z"
+                  class="nordost background"/>
+        </g>
+        <g class="stadtteil nord">
+            <path stroke-width="4" d="M566.5 171l10.5-60-97-25.5-49.5-69-95-13.5-70 93.5 148.5 132L566.5 171z"
+                  class="nord background"/>
+        </g>
+        <g class="stadtteil ost">
+            <path stroke-width="4" d="M680 316l-11-85.922L798.5 205.5l71 44 106.5-21 23 96-66.5 34-38 72L680 316z"
+                  class="ost background"/>
+        </g>
+        <g class="stadtteil suedost">
+            <path stroke-width="4" d="M680 316l-71 42.5 96.5 196 176-42 13-82L680 316z"
+                  class="suedost background"/>
+        </g>
+        <g class="stadtteil sued">
+            <path stroke-width="4" d="M429 293l102.5-10 77.5 75.5 96.5 196L429 465l-113.5 38.5L429 293z"
+                  class="sued background"/>
+        </g>
+        <g class="stadtteil zentrum">
+            <path stroke-width="4" d="M566.5 171L669 230.078 680 316l-71 42.5-77.5-75.5L429 293l-15-64.5L566.5 171z"
+                  class="zentrum background"/>
+        </g>
+    </g>
+</svg>
