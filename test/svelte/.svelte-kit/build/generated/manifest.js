7ae894d247c152f894bfbb3e7eb55a916fa5133a
cutmeat <johannes.rave@gmail.com>
2021-05-24 01:57:35 +0200

    imported svg-pan-zoom, but map is static as of yet

--- test/svelte/.svelte-kit/build/generated/manifest.js
+++ test/svelte/.svelte-kit/build/generated/manifest.js
@@ -2,8 +2,7 @@ const c = [
 	() => import("..\\..\\..\\src\\routes\\__layout.svelte"),
 	() => import("..\\components\\error.svelte"),
 	() => import("..\\..\\..\\src\\routes\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\search\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\about.svelte"),
+	() => import("..\\..\\..\\src\\routes\\search.svelte"),
 	() => import("..\\..\\..\\src\\routes\\shop.svelte")
 ];
 
@@ -13,14 +12,11 @@ export const routes = [
 	// src/routes/index.svelte
 	[/^\/$/, [c[0], c[2]], [c[1]]],
 
-	// src/routes/search/index.svelte
+	// src/routes/search.svelte
 	[/^\/search\/?$/, [c[0], c[3]], [c[1]]],
 
-	// src/routes/about.svelte
-	[/^\/about\/?$/, [c[0], c[4]], [c[1]]],
-
 	// src/routes/shop.svelte
-	[/^\/shop\/?$/, [c[0], c[5]], [c[1]]]
+	[/^\/shop\/?$/, [c[0], c[4]], [c[1]]]
 ];
 
 export const fallback = [c[0](), c[1]()];
