ff3fe3562bbada1606c32bd82e5b0109f25d6373
cutmeat <johannes.rave@gmail.com>
2021-06-19 12:26:09 +0200

    working materialify-test, sieht zu umfangreich aus. ich versuche es mit attractions.

---
+++ materialify-test/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { MaterialApp } from 'svelte-materialify';
+    let theme = 'light';
+
+    function toggleTheme() {
+        if (theme === 'light') theme = 'dark';
+        else theme = 'light';
+    }
+</script>
+
+<MaterialApp {theme}>
+    <button on:click="{toggleTheme}">Toggle Theme</button>
+    ...
+</MaterialApp>
