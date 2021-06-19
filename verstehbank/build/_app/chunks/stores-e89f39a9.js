fad9aba38c47e70e84812a5db30a2733346481d1
cutmeat <johannes.rave@gmail.com>
2021-06-20 01:15:44 +0200

    Versuch, base path mit base zu setzen - hat nicht funktioniert. bleibenlassen!

--- verstehbank/build/_app/chunks/stores-e89f39a9.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./vendor-93950e72.js";const r=o(!1);export{r as a};
--- verstehbank/build/_app/pages/contacts/index.svelte-6b06d360.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as e,s as r}from"../../chunks/vendor-93950e72.js";export default class extends s{constructor(s){super(),e(this,s,null,null,r,{})}}
